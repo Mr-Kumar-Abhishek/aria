0d8ae4ac1da573af69cfb95be6375fdf26609697
ukari <muromi.ukari@gmail.com>
2019-03-01 23:48:31 +0900

    update

--- src/control/rx.lisp
+++ src/control/rx.lisp
@@ -4,7 +4,11 @@
   (:use :cl)
   (:export :observable
            :observer
-           :suscribe
+           :subject
+           :onnext
+           :onfail
+           :onover
+           :subscribe
            :filter))
 
 (in-package :aria.control.rx)
@@ -26,36 +30,47 @@
            :type function)))
 
 (defclass subject (observer)
-  ((observables :initform nil
-                :accessor observables
+  ((observers :initform nil
+                :accessor observers
                 :type list)))
 
+(defmethod id (x) x)
 
 (defmethod observable ((revolver function))
   (make-instance 'observable :revolver revolver))
 
-(defmethod id (x) x)
-
 (defmethod observer (&key (onnext #'id) (onfail #'id) (onover #'id))
   (make-instance 'observer :onnext onnext :onfail onfail :onover onover))
 
-(defmethod filter ((self observable) (filter function))
-  (make-instance 'observable
-                 :revolver
-                 (lambda (observer)
-                   (subscribe self (observer :onnext (lambda (value) (if (funcall filter value)
-                                                                         (funcall (onnext observer) value)))
-                                             :onfail (onfail observer)
-                                             :onover (onover observer))))))
+(defmethod broadcast ((self subject) (method function))
+  (lambda (&rest args)
+    (map nil (lambda (observer) (apply (funcall method observer) args)) (observers self))))
+
+(defmethod subject ()
+  (let ((self (make-instance 'subject)))
+    (setf (onnext self) (broadcast self #'onnext))
+    (setf (onfail self) (broadcast self #'onfail))
+    (setf (onover self) (broadcast self #'onover))
+    self))
 
 (defmethod subscribe ((self observable) (ob observer))
   (funcall (revolver self) ob))
 
 (defmethod subscribe ((self subject) (ob observer))
-  )
+  (push ob (observers self))
+  self)
 
 (defmethod subscribe ((self observable) (onnext function))
-  )
+  (subscribe self (observer :onnext onnext)))
 
 (defmethod subscribe ((self subject) (onnext function))
-  )
+  (subscribe self (observer :onnext onnext)))
+
+(defmethod filter ((self observable) (predicate function))
+  (make-instance 'observable
+                 :revolver
+                 (lambda (observer)
+                   (subscribe self (observer :onnext (lambda (value) (if (funcall predicate value)
+                                                                         (funcall (onnext observer) value)))
+                                             :onfail (onfail observer)
+                                             :onover (onover observer))))))
